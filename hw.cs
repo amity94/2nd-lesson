d0ba55473304adb2004726f531cfd70553e1656e
amity94 <45635119+amity94@users.noreply.github.com>
2018-12-05 18:49:37 +0200

    Add files via upload

---
+++ hw.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            question1();
+        }
+
+        private static void question1()
+        {
+            Console.WriteLine("enter number of students in class");
+            int students = int.Parse(Console.ReadLine());
+
+            int[] gradesarray = new int[students];
+            for (int i = 0; i < gradesarray.Length; i++)
+            {
+                gradesarray[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            int sum = 0;
+            for (int i = 0; i < gradesarray.Length; i++)
+            {
+                sum = sum + gradesarray[i];
+
+            }
+            double avarage = sum / gradesarray.Length;
+            Console.WriteLine("the avarage was" + avarage);
+        }
+
+         
+
+
+
+
+
+
+    }
+
+}
